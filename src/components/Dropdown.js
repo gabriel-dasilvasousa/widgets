03dbc4286e7955d185d12966a671799ae1c4abb8
Gabriel da Silva Sousa <g.silva13455@gmail.com>
2020-10-09 21:17:44 -0300

    Close dropdown with click in body

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();
     
+    useEffect(() => {
+        document.body.addEventListener('click', (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+            setOpen(false);
+        });
+    });
+
     const renderedOptions = options.map((option) => {
         if(option.label === selected.label){
             return null;
@@ -19,7 +29,7 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
     });
 
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
                 <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
